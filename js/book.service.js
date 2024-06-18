4af636febaf9be13fc8e42287fc856f6ae6b87d1
Nir <nirlevi5288@gmail.com>
2024-06-18 19:27:11 +0300

    Init repo

---
+++ js/book.service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var gBooks = [
+    { id: 'bk101', title: 'The Great Gatsby', price: 15, imgUrl: 'great-gatsby.jpg' },
+    { id: 'bk102', title: '1984', price: 12, imgUrl: '1984.jpg' },
+    { id: 'bk103', title: 'To Kill a Mockingbird', price: 18, imgUrl: 'to-kill-a-mockingbird.jpg' }
+]
+
+function getBooks() {
+    return gBooks
+}
