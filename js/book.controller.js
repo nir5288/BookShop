dfda9dfc536bdef6c6068b81d7a5914987bae78d
Nir <nirlevi5288@gmail.com>
2024-06-18 20:03:03 +0300

    Render book list dynamic and update html

--- js/book.controller.js
+++ js/book.controller.js
@@ -3,3 +3,15 @@
 function onInit() {
     renderBooks()
 }
+
+function renderBooks() {
+    const elBookList = document.querySelector('.book-list')
+    const strHtmls = getBooks().map(book => 
+            `<tr>
+                <td>${book.title}</td>
+                <td>$${book.price}</td>
+                <td><button>read</button><button>update</button><button>delete</button></td>
+            </tr>`
+    )
+    elBookList.innerHTML = strHtmls.join('')
+}
